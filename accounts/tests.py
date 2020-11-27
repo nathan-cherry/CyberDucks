8decf3dd148f3b3d648ffe9e058c92f3624df23e
nathan-cherry <cherryn@uwindsor.ca>
2020-11-27 13:06:56 -0500

    Nathan UnitTest

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,5 @@
+from urllib.parse import urlencode
+
 from django.test import TestCase
 from .models import *
 
@@ -27,12 +29,35 @@ class TaskTestCase(TestCase):
 
 # Nathan
 class AccountCreationTestCase(TestCase):
-    def test_create_user(self):
+    def setUp(self):
+        # credentials
+        self.credentials = {
+            'username': 'test_user',
+            'password': 'secret'
+        }
         # Create user
-        self.user = User.objects.create_user(username='test_user', password='12345')
+        user = User.objects.create_user(**self.credentials)
         # Log user in
-        login = self.client.login(username='test_user', password='12345')
+        login = self.client.login(username='test_user', password='secret')
         # Checks if login successful
         self.assertEqual(login, True)
         # Check if logged in user is the newly created user
-        self.assertEqual(int(self.client.session['_auth_user_id']), self.user.pk)
+        self.assertEqual(int(self.client.session['_auth_user_id']), user.pk)
+
+    def test_login(self):
+        # This will send the credentials to the '/login/' path (urls.py) which calls the loginPage function in views.py
+        # since the request method is post, the function will attempt to login the user
+        data = urlencode({'username': 'test_user', 'password': 'secret'})
+        response = self.client.post('/login/', data, content_type="application/x-www-form-urlencoded", follow=True)
+        # Test that the authentication worked and the user was created and signed in
+        self.assertEqual(response.context['user'].is_authenticated, True)
+
+    def test_logout(self):
+        # Log in to user
+        login = self.client.login(username='test_user', password='secret')
+        # Check that login successful
+        self.assertEqual(login, True)
+        # Send request to the logout method
+        response = self.client.get('/logout/', follow=True)
+        # Check that the user is not authenticated
+        self.assertEqual(response.context['user'].is_authenticated, False)
